6388800a66fbb87d1891bbbbab0c87187c81f34e
jeremygovi <jeremygovi@gmail.com>
2022-01-05 10:40:05 +0100

    fix: "Role with name api_gateway_cloudwatch_global already exists" error + hello.zip path

--- s3_bucket_codebase_zip.tf
+++ s3_bucket_codebase_zip.tf
@@ -2,6 +2,7 @@
 resource "aws_s3_bucket_object" "code_base_package" {
   bucket = var.lambda_codebase_bucket
   key    = "${var.lambda_project_name}.zip"
-  #source = "hello.zip"
-  source = "../hello.zip"
+  source = "hello.zip"
+  # switch to this source for local module testing (path are different if we are in local or remote)
+  #source = "../hello.zip"
 }
