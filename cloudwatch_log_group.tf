e348c7eb257cf42373e7d5955d94c77af53d526b
jeremygovi <jeremygovi@gmail.com>
2022-01-07 17:42:26 +0100

    change loggroup path to correspond to the path lambda is logging

--- cloudwatch_log_group.tf
+++ cloudwatch_log_group.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "lambda_aws_cloudwatch_log_group" {
   for_each          = toset(var.api_gateway_stages)
-  name              = "/aws/lambda/${each.key}/${var.lambda_project_name}"
+  name              = "/aws/lambda/${var.lambda_project_name}_${each.key}"
   retention_in_days = var.lambda_cloudwatch_logs_retention
   tags = merge({
     module           = "apigw_lambda",
@@ -10,7 +10,7 @@ resource "aws_cloudwatch_log_group" "lambda_aws_cloudwatch_log_group" {
 
 resource "aws_cloudwatch_log_group" "api_gateway_aws_cloudwatch_log_group" {
   for_each          = var.use_api_gateway == true ? toset(var.api_gateway_stages) : []
-  name              = "/aws/apigateway/${each.key}/${var.lambda_project_name}"
+  name              = "/aws/apigateway/${var.lambda_project_name}_${each.key}"
   retention_in_days = var.api_gateway_cloudwatch_logs_retention
   tags = merge({
     module           = "apigw_lambda",
