dca7a1afd4f52ec7d28749ed1468dada37cd575a
Jérôme CHAMOIN <jerome.chamoin@openclassrooms.com>
2023-01-12 15:57:58 +0100

    rename resource name

--- sqs.tf
+++ sqs.tf
@@ -3,7 +3,7 @@ locals {
   sqs_queues = var.sqs_enabled == true ? toset(var.sqs_queues_arn) : []
 }
 
-resource "aws_lambda_event_source_mapping" "example" {
+resource "aws_lambda_event_source_mapping" "sqs_aws_lambda_event_source_mapping" {
   for_each         = local.sqs_queues
   event_source_arn = each.value
   function_name    = var.lambda_project_name
